abdaa82a1deddf7e059fa966e2fbfe5408393c27
mark <mark@mdmorris.co.uk>
2017-09-29 10:06:20 +0100

    Started unit tests on urls being present

--- mdmorris/tests/test_urls.py
+++ mdmorris/tests/test_urls.py
@@ -3,6 +3,17 @@ from django.test import TestCase
 
 class PageExistTest(TestCase):
 
+
+	def exists_or_redirect(self, status_code):
+		true_or_false = (status_code == 200 or status_code == 302)
+		if true_or_false == False:
+			print(status_code)
+		return true_or_false
+
 	def test_home_page_exists(self):
 		response = self.client.get('/')
-		self.assertEqual(response.status_code, 200)
+		self.assertTrue(self.exists_or_redirect(response.status_code))
+
+	def test_about_me_page_exists(self):
+		response = self.client.get('/about-me')
+		self.assertTrue(self.exists_or_redirect(response.status_code))
