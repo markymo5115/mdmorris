948857d7c3e060d6f55ae4a974ebb65664cb2042
mark <mark@mdmorris.co.uk>
2017-09-29 11:17:13 +0100

    About Mark page now exists with an aboutme template

--- mdmorris/urls.py
+++ mdmorris/urls.py
@@ -15,16 +15,17 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from aboutme import views as aboutme_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', admin.site.urls),
-    url(r'^about-me/', admin.site.urls),
-    url(r'^gallery/', admin.site.urls),
-    url(r'^how-to-order/', admin.site.urls),
-    url(r'^blog/', admin.site.urls),
-    url(r'^contact-mark/', admin.site.urls),
-    url(r'^links/', admin.site.urls),
-    url(r'^buy-a-necklace/', admin.site.urls),
-    url(r'^view-cart/', admin.site.urls),
+    url(r'^about-me', aboutme_views.view_about_me, name="about_me"),
+    url(r'^gallery', admin.site.urls),
+    url(r'^how-to-order', admin.site.urls),
+    url(r'^blog', admin.site.urls),
+    url(r'^contact-mark', admin.site.urls),
+    url(r'^links', admin.site.urls),
+    url(r'^buy-a-necklace', admin.site.urls),
+    url(r'^view-cart', admin.site.urls),
 ]
